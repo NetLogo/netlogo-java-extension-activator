834df0ba9d4f6be445c0b8806d41c0c1294503be
arthurhjorth <arthur.hjorth@u.northwestern.edu>
2016-04-21 12:38:54 -0500

    Added wiggle

--- src/main/HelloJavaExtension.java
+++ src/main/HelloJavaExtension.java
@@ -1,3 +1,5 @@
+package main;
+
 import org.nlogo.api.*;
 import org.nlogo.core.Syntax;
 import org.nlogo.core.SyntaxJ;
@@ -5,6 +7,7 @@ import org.nlogo.core.SyntaxJ;
 public class HelloJavaExtension extends DefaultClassManager {
     public void load(PrimitiveManager manager) {
         manager.addPrimitive("hello", new HelloString());
+        manager.addPrimitive("wiggle", new Wiggle());
     }
 
     public static class HelloString implements Reporter {
@@ -16,7 +19,21 @@ public class HelloJavaExtension extends DefaultClassManager {
         @Override
         public Object report(Argument[] args, Context context) throws ExtensionException {
             String name = args[0].getString();
-            return "hello, " + name;
+            return "hello, " + name + "y Mc"+name+"face";
         }
+    }
+    public static class Wiggle implements Command {
+        @Override
+        public Syntax getSyntax() {
+            return SyntaxJ.commandSyntax(new int[] {});
+        }
+
+        @Override
+        public void perform(Argument[] args, Context context) throws ExtensionException {
+            Turtle anAgent;
+            anAgent = (Turtle)context.getAgent();
+            anAgent.heading(anAgent.heading() + Math.random() * 25 - Math.random() * 25);
+        }
+
     }
 }
